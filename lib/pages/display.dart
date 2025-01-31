9196a28605b00c704c2e64c10863b61718e257f2
mirei <ovamirei@gmail.com>
2025-01-31 18:05:54 +0700

    ModifyBottomNav

--- lib/pages/display.dart
+++ lib/pages/display.dart
@@ -9,6 +9,8 @@ class DisplayPage extends StatefulWidget {
 }
 
 class _DisplayPageState extends State<DisplayPage> {
+                  var i = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,22 +25,31 @@ class _DisplayPageState extends State<DisplayPage> {
                 child: CircularProgressIndicator(),
               );
             }
+
             return ListView(
               children: snapshot.data!.docs.map((document) {
-                return ListTile(
-                  leading: CircleAvatar(
-                    backgroundColor: Colors.lightBlue,
-                    radius: 30,
-                    child: FittedBox(
-                      child: Text(
-                        document["score"],
-                        style: TextStyle(fontSize: 20, color: Colors.white),
+                return Card(
+                  margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  elevation: 10,
+                  shadowColor: Colors.blueAccent.withValues(alpha: 0.3),
+                  child: Padding(
+                    padding: const EdgeInsets.all(5),
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        backgroundColor: Colors.lightBlue,
+                        radius: 30,
+                        child: FittedBox(
+                          child: Text(
+                            document["score"],
+                            style: TextStyle(fontSize: 20, color: Colors.white),
+                          ),
+                        ),
                       ),
+                      // ignore: prefer_interpolation_to_compose_strings
+                      title: Text(document["fname"] +' '+ document["lname"],  style: TextStyle(fontSize: 20, color: Colors.black)),
+                      subtitle: Text(document["email"], style: TextStyle(fontSize: 18, color: Colors.grey)),
                     ),
                   ),
-                  // ignore: prefer_interpolation_to_compose_strings
-                  title: Text(document["fname"] +' '+ document["lname"],  style: TextStyle(fontSize: 20, color: Colors.black)),
-                  subtitle: Text(document["email"], style: TextStyle(fontSize: 18, color: Colors.grey)),
                 );
               }).toList(),
             );
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,25 +10,45 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _index = 0;
+  List pages = [
+    FormPage(),
+    DisplayPage(),
+  ];
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        body: TabBarView(
-          children: [
-            FormPage(),
-            DisplayPage(),
-          ],
-        ),
-        backgroundColor: Colors.lightBlue,
-        bottomNavigationBar: TabBar(
-          tabs: [
-            Tab(text: "บันทึกคะแนนสอบ",),
-            Tab(text: "รายชื่อนักเรียน",),
-          ],
+    return Scaffold(
+      bottomNavigationBar: Container(
+        margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        decoration: BoxDecoration(boxShadow: [
+          BoxShadow(
+              color: Colors.black12, blurRadius: 20, offset: Offset(0, 10))
+        ]),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(50),
+          child: BottomNavigationBar(
+            currentIndex: _index,
+            backgroundColor: Colors.white,
+            selectedItemColor: Colors.lightBlue,
+            unselectedItemColor: Colors.black,
+            selectedFontSize: 18,
+            showSelectedLabels: true,
+            showUnselectedLabels: false,
+            onTap: (index) {
+              setState(() {
+                _index = index;
+              });
+            },
+            items: [
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.edit_document), label: "บันทึกคะแนนสอบ"),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.people), label: "รายชื่อนักเรียน"),
+            ],
+          ),
         ),
       ),
+      body: pages[_index],
     );
   }
 }
